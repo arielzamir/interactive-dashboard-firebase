02b8f5661573b2d06dd76069ef6b31dee483e421
Ariel Zamir <arielzamir100@gmail.com>
2025-06-02 23:10:48 +0300

    created a new project on Firebase and changed accordingly

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,15 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import express from "express";
 import cors from "cors";
 import {trafficStatsApi} from "./routes/trafficStats";
 
 const app = express();
 
-app.use(express.json());
 app.use(cors({origin: true}));
+app.use(express.json());
 
 app.use("/api", trafficStatsApi);
 
-export const trafficStats = functions.https.onRequest(app);
+export const trafficStats = functions
+  .region("us-central1")
+  .https.onRequest(app);
