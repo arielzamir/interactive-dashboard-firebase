85c6081a015d80fa9af449f43a4c310192969aaa
arielzamir <Ariel.Z@cortexre.com>
2025-07-06 17:25:32 +0300

    Remove firebase-debug.log from repo, and configured cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -24,7 +24,7 @@ if (missingEssentialEnvVariables.length > 0) {
 const app = express();
 
 const corsOptions = {
-  origin: true,
+  origin: ["http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
