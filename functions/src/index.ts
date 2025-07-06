45f25af30a5ec29d51a7594ca50fe2cdd397f1da
arielzamir <Ariel.Z@cortexre.com>
2025-07-06 18:56:16 +0300

    Enabled the prod url for cors middleware

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -24,7 +24,7 @@ if (missingEssentialEnvVariables.length > 0) {
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://fir-project-d9b5e.web.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
